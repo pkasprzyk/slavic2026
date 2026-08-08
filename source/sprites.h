0ef5ab24a5cebd5ac593401302144bfca88c818f
Lukasz Zephyr <s-zephyr@o2.pl>
2026-08-08 17:05:43 +0200

    jumping bunnies on top screen

--- source/sprites.h
+++ source/sprites.h
@@ -9,27 +9,32 @@
 
 #include "ids.h"
 
+enum SCREENS_TO_LOAD
+{
+  TOP = 1,
+  BOTTOM = 2,
+  BOTH = TOP | BOTTOM
+};
+
 typedef struct {
   const char *path;
   uint16_t width;
   uint16_t height;
   uint16_t img_id;
   uint16_t pal_id;
-  bool is_top_screen;
+  u16 screens;
 } SpriteInfo;
 
-enum { MECH = 0, RABBITS = 1, REACTOR_CORE = 2, FIRE = 3, WATER_DROPS = 4 };
+enum { MECH = 0, RABBITS = 1, FIRE = 2, WATER_DROPS = 3, SPRITE_CNT };
 
-#define SPRITE_CNT 5
 
 #define DEFAULT_SPRITE_PALETTE 0
 
 static const SpriteInfo SPRITE_INFOS[SPRITE_CNT] = {
-    [MECH] = {"spr/sprite_robot_all", 32, 32, MECH, DEFAULT_SPRITE_PALETTE, false},
-    [RABBITS] = {"spr/sprite_hare_idle", 16, 16, RABBITS, DEFAULT_SPRITE_PALETTE, false},
-    [REACTOR_CORE] = {"spr/reactor_core", 32, 32, REACTOR_CORE, DEFAULT_SPRITE_PALETTE, true},
-    [FIRE] = {"spr/sprite_fire_small", 8, 8, FIRE, DEFAULT_SPRITE_PALETTE, false},
-    [WATER_DROPS] = {"spr/sprite_water_drops", 16, 16, WATER_DROPS, DEFAULT_SPRITE_PALETTE, false},
+    [MECH] = {"spr/sprite_robot_all", 32, 32, MECH, DEFAULT_SPRITE_PALETTE, BOTTOM},
+    [RABBITS] = {"spr/sprite_hare_idle", 16, 16, RABBITS, DEFAULT_SPRITE_PALETTE, BOTH},
+    [FIRE] = {"spr/sprite_fire_small", 8, 8, FIRE, DEFAULT_SPRITE_PALETTE, BOTTOM},
+    [WATER_DROPS] = {"spr/sprite_water_drops", 16, 16, WATER_DROPS, DEFAULT_SPRITE_PALETTE, BOTTOM},
 };
 
 void InitSprites(void);
