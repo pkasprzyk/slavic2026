e6ff505897d214f204a476f639c3ae4b1d3ceb02
Grafiszti <grafiszti@gmail.com>
2026-08-08 17:51:36 +0200

    arrows indicators

--- source/bunny.c
+++ source/bunny.c
@@ -16,6 +16,7 @@ typedef struct {
   s16 x;
   s16 y;
   s16 oam_id;
+  s16 indicator_oam_id;
 } bunny_s;
 
 static bunny_s bunnies[BUNNIES_MAX];
@@ -47,6 +48,7 @@ static u16 frame_cnt = 0;
 
 void add_bunny(int x, int y) {
   bunnies[bunnies_cnt].oam_id = BUNNIES_OAM_ID + bunnies_cnt;
+  bunnies[bunnies_cnt].indicator_oam_id = INDICATOR_OAM_BASE + bunnies_cnt;
   bunnies[bunnies_cnt].x = x;
   bunnies[bunnies_cnt].y = y;
   ++bunnies_cnt;
@@ -63,6 +65,12 @@ void bunnies_init(void) {
     NF_CreateSprite(SCR_WORLD, bunnies[i].oam_id, SPRITE_INFOS[RABBITS].img_id,
                     SPRITE_INFOS[RABBITS].pal_id, bunnies[i].x, bunnies[i].y);
     NF_SpriteLayer(SCR_WORLD, bunnies[i].oam_id, LAYER_WORLD_BG);
+
+    NF_CreateSprite(SCR_WORLD, bunnies[i].indicator_oam_id,
+                    SPRITE_INFOS[INDICATOR_ARROW].img_id,
+                    SPRITE_INFOS[INDICATOR_ARROW].pal_id, 0, 0);
+    NF_SpriteLayer(SCR_WORLD, bunnies[i].indicator_oam_id, LAYER_WORLD_BG);
+    NF_ShowSprite(SCR_WORLD, bunnies[i].indicator_oam_id, false);
   }
 }
 
@@ -81,6 +89,7 @@ bool collides_with_mech(u16 i) {
 bool collect(u16 bunnyId) {
   audio_play_sfx(SFX_SFX_BUNNY_PICK_UP, false, IGNORED_LEN, DEFAULT_VOLUME);
   NF_DeleteSprite(SCR_WORLD, bunnies[bunnyId].oam_id);
+  NF_DeleteSprite(SCR_WORLD, bunnies[bunnyId].indicator_oam_id);
   bunnies[bunnyId] = bunnies[bunnies_cnt - 1];
   --bunnies_cnt;
 
@@ -91,6 +100,68 @@ bool collect(u16 bunnyId) {
   return true;
 }
 
+static void compute_edge_indicator(s16 bunny_wx, s16 bunny_wy,
+                                     s16 *out_x, s16 *out_y,
+                                     int *out_frame, bool *out_hflip, bool *out_vflip) {
+  s16 cx = mech_x - level_cam_x();
+  s16 cy = mech_y - level_cam_y();
+  s16 dx = bunny_wx - level_cam_x() - cx;
+  s16 dy = bunny_wy - level_cam_y() - cy;
+
+  if (dx == 0 && dy == 0) {
+    *out_x = cx; *out_y = cy;
+    *out_frame = 2; *out_hflip = false; *out_vflip = false;
+    return;
+  }
+
+  s16 best_t = 0x7FFF;
+
+  if (dx < 0) {
+    s16 t = ((-cx) << 8) / dx;
+    s16 y = cy + ((t * dy) >> 8);
+    if (y >= 0 && y < SCREEN_H && t >= 0 && t < best_t) best_t = t;
+  } else if (dx > 0) {
+    s16 t = ((SCREEN_W - 1 - cx) << 8) / dx;
+    s16 y = cy + ((t * dy) >> 8);
+    if (y >= 0 && y < SCREEN_H && t >= 0 && t < best_t) best_t = t;
+  }
+
+  if (dy < 0) {
+    s16 t = ((-cy) << 8) / dy;
+    s16 x = cx + ((t * dx) >> 8);
+    if (x >= 0 && x < SCREEN_W && t >= 0 && t < best_t) best_t = t;
+  } else if (dy > 0) {
+    s16 t = ((SCREEN_H - 1 - cy) << 8) / dy;
+    s16 x = cx + ((t * dx) >> 8);
+    if (x >= 0 && x < SCREEN_W && t >= 0 && t < best_t) best_t = t;
+  }
+
+  *out_x = cx + ((best_t * dx) >> 8);
+  *out_y = cy + ((best_t * dy) >> 8);
+
+  if (*out_x < 0) *out_x = 0;
+  if (*out_x >= SCREEN_W) *out_x = SCREEN_W - 1;
+  if (*out_y < 0) *out_y = 0;
+  if (*out_y >= SCREEN_H) *out_y = SCREEN_H - 1;
+
+  s16 adx = dx < 0 ? -dx : dx;
+  s16 ady = dy < 0 ? -dy : dy;
+
+  if (ady * 5 < adx * 2) {
+    *out_frame = 0;
+    *out_hflip = (dx < 0);
+    *out_vflip = false;
+  } else if (adx * 5 < ady * 2) {
+    *out_frame = 1;
+    *out_hflip = false;
+    *out_vflip = (dy < 0);
+  } else {
+    *out_frame = 2;
+    *out_hflip = (dx < 0);
+    *out_vflip = (dy < 0);
+  }
+}
+
 void bunnies_update(void) {
   ++frame_cnt;
   frame_cnt %= 60 * 2;
@@ -99,21 +170,32 @@ void bunnies_update(void) {
       collect(i);
       continue;
     }
-    s16 phaseSin = sinLerp((frame_cnt - 60) * (32767 / 60)); // 4.12 fixed
+    s16 phaseSin = sinLerp((frame_cnt - 60) * (32767 / 60));
     s16 offset = (3 * phaseSin) >> 12;
     s16 x = bunnies[i].x - level_cam_x();
     s16 y = bunnies[i].y - level_cam_y() + offset;
-    if (x < -16 || x > 256 || y < -16 || y > 192) // off screen
-    {
+    if (x < -16 || x > 256 || y < -16 || y > 192) {
       NF_ShowSprite(SCR_WORLD, bunnies[i].oam_id, false);
+
+      s16 ex, ey;
+      int frame;
+      bool hflip, vflip;
+      compute_edge_indicator(bunnies[i].x, bunnies[i].y,
+                             &ex, &ey, &frame, &hflip, &vflip);
+      NF_ShowSprite(SCR_WORLD, bunnies[i].indicator_oam_id, true);
+      NF_MoveSprite(SCR_WORLD, bunnies[i].indicator_oam_id, ex, ey);
+      NF_SpriteFrame(SCR_WORLD, bunnies[i].indicator_oam_id, frame);
+      NF_HflipSprite(SCR_WORLD, bunnies[i].indicator_oam_id, hflip);
+      NF_VflipSprite(SCR_WORLD, bunnies[i].indicator_oam_id, vflip);
       continue;
     }
+    NF_ShowSprite(SCR_WORLD, bunnies[i].indicator_oam_id, false);
     NF_ShowSprite(SCR_WORLD, bunnies[i].oam_id, true);
     NF_MoveSprite(SCR_WORLD, bunnies[i].oam_id, x, y);
     NF_SpriteFrame(SCR_WORLD, bunnies[i].oam_id, frame_cnt / 30);
   }
   for (s16 i = 0 ; i < bunnies_collected; ++i) {
-    s16 phaseSin = sinLerp((frame_cnt - 60) * (32767 / 60)); // 4.12 fixed
+    s16 phaseSin = sinLerp((frame_cnt - 60) * (32767 / 60));
     s16 offset = (10 * phaseSin) >> 12;
     s16 x = savedSlots[i].x;
     s16 y = savedSlots[i].y + offset;
--- source/sprites.h
+++ source/sprites.h
@@ -25,7 +25,7 @@ typedef struct {
   u16 screens;
 } SpriteInfo;
 
-enum { MECH = 0, RABBITS = 1, FIRE = 2, WATER_DROPS = 3, SPRITE_CNT };
+enum { MECH = 0, RABBITS = 1, FIRE = 2, WATER_DROPS = 3, INDICATOR_ARROW = 4, SPRITE_CNT };
 
 
 #define DEFAULT_SPRITE_PALETTE 0
@@ -35,6 +35,7 @@ static const SpriteInfo SPRITE_INFOS[SPRITE_CNT] = {
     [RABBITS] = {"spr/sprite_hare_idle", 16, 16, RABBITS, DEFAULT_SPRITE_PALETTE, BOTH},
     [FIRE] = {"spr/sprite_fire_small", 8, 8, FIRE, DEFAULT_SPRITE_PALETTE, BOTTOM},
     [WATER_DROPS] = {"spr/sprite_water_drops", 16, 16, WATER_DROPS, DEFAULT_SPRITE_PALETTE, BOTTOM},
+    [INDICATOR_ARROW] = {"spr/indicator_arrow", 8, 8, INDICATOR_ARROW, DEFAULT_SPRITE_PALETTE, BOTTOM},
 };
 
 void InitSprites(void);
@@ -47,6 +48,9 @@ void InitSprites(void);
 
 #define WATER_DROP_OAM_BASE 11
 #define WATER_DROP_MAX 5
+
+#define INDICATOR_OAM_BASE 16
+#define INDICATOR_MAX BUNNIES_MAX
 
 
 #endif
