03350f93343f63c8d73be7fd2bc0fb5285e0476e
Grafiszti <grafiszti@gmail.com>
2026-08-08 19:30:37 +0200

    tilesets

---
+++ assets/tiles/forest_tiles.tsx
@@ -0,0 +1,340 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2-2-geb83ddb9" name="forest_tiles" tilewidth="8" tileheight="8" tilecount="110" columns="0">
+ <tile id="0">
+  <image source="tile_0000.png" width="8" height="8"/>
+ </tile>
+ <tile id="1">
+  <image source="tile_0001.png" width="8" height="8"/>
+ </tile>
+ <tile id="2">
+  <image source="tile_0002.png" width="8" height="8"/>
+ </tile>
+ <tile id="3">
+  <image source="tile_0003.png" width="8" height="8"/>
+ </tile>
+ <tile id="4">
+  <image source="tile_0004.png" width="8" height="8"/>
+ </tile>
+ <tile id="5">
+  <image source="tile_0005.png" width="8" height="8"/>
+ </tile>
+ <tile id="6">
+  <image source="tile_0006.png" width="8" height="8"/>
+ </tile>
+ <tile id="7">
+  <image source="tile_0007.png" width="8" height="8"/>
+ </tile>
+ <tile id="8">
+  <image source="tile_0008.png" width="8" height="8"/>
+ </tile>
+ <tile id="9">
+  <image source="tile_0009.png" width="8" height="8"/>
+ </tile>
+ <tile id="10">
+  <image source="tile_0010.png" width="8" height="8"/>
+ </tile>
+ <tile id="11">
+  <image source="tile_0011.png" width="8" height="8"/>
+ </tile>
+ <tile id="12">
+  <image source="tile_0012.png" width="8" height="8"/>
+ </tile>
+ <tile id="13">
+  <image source="tile_0013.png" width="8" height="8"/>
+ </tile>
+ <tile id="14">
+  <image source="tile_0014.png" width="8" height="8"/>
+ </tile>
+ <tile id="15">
+  <image source="tile_0015.png" width="8" height="8"/>
+ </tile>
+ <tile id="16">
+  <image source="tile_0016.png" width="8" height="8"/>
+ </tile>
+ <tile id="17">
+  <image source="tile_0017.png" width="8" height="8"/>
+ </tile>
+ <tile id="18">
+  <image source="tile_0018.png" width="8" height="8"/>
+ </tile>
+ <tile id="19">
+  <image source="tile_0019.png" width="8" height="8"/>
+ </tile>
+ <tile id="20">
+  <image source="tile_0020.png" width="8" height="8"/>
+ </tile>
+ <tile id="21">
+  <image source="tile_0021.png" width="8" height="8"/>
+ </tile>
+ <tile id="22">
+  <image source="tile_0022.png" width="8" height="8"/>
+ </tile>
+ <tile id="23">
+  <image source="tile_0023.png" width="8" height="8"/>
+ </tile>
+ <tile id="24">
+  <image source="tile_0024.png" width="8" height="8"/>
+ </tile>
+ <tile id="25">
+  <image source="tile_0025.png" width="8" height="8"/>
+ </tile>
+ <tile id="26">
+  <image source="tile_0026.png" width="8" height="8"/>
+ </tile>
+ <tile id="27">
+  <image source="tile_0027.png" width="8" height="8"/>
+ </tile>
+ <tile id="28">
+  <image source="tile_0028.png" width="8" height="8"/>
+ </tile>
+ <tile id="29">
+  <image source="tile_0029.png" width="8" height="8"/>
+ </tile>
+ <tile id="30">
+  <image source="tile_0030.png" width="8" height="8"/>
+ </tile>
+ <tile id="31">
+  <image source="tile_0031.png" width="8" height="8"/>
+ </tile>
+ <tile id="32">
+  <image source="tile_0032.png" width="8" height="8"/>
+ </tile>
+ <tile id="33">
+  <image source="tile_0033.png" width="8" height="8"/>
+ </tile>
+ <tile id="34">
+  <image source="tile_0034.png" width="8" height="8"/>
+ </tile>
+ <tile id="35">
+  <image source="tile_0035.png" width="8" height="8"/>
+ </tile>
+ <tile id="36">
+  <image source="tile_0036.png" width="8" height="8"/>
+ </tile>
+ <tile id="37">
+  <image source="tile_0037.png" width="8" height="8"/>
+ </tile>
+ <tile id="38">
+  <image source="tile_0038.png" width="8" height="8"/>
+ </tile>
+ <tile id="39">
+  <image source="tile_0039.png" width="8" height="8"/>
+ </tile>
+ <tile id="40">
+  <image source="tile_0040.png" width="8" height="8"/>
+ </tile>
+ <tile id="41">
+  <image source="tile_0041.png" width="8" height="8"/>
+ </tile>
+ <tile id="42">
+  <image source="tile_0042.png" width="8" height="8"/>
+ </tile>
+ <tile id="43">
+  <image source="tile_0043.png" width="8" height="8"/>
+ </tile>
+ <tile id="44">
+  <image source="tile_0044.png" width="8" height="8"/>
+ </tile>
+ <tile id="45">
+  <image source="tile_0045.png" width="8" height="8"/>
+ </tile>
+ <tile id="46">
+  <image source="tile_0046.png" width="8" height="8"/>
+ </tile>
+ <tile id="47">
+  <image source="tile_0047.png" width="8" height="8"/>
+ </tile>
+ <tile id="48">
+  <image source="tile_0048.png" width="8" height="8"/>
+ </tile>
+ <tile id="49">
+  <image source="tile_0049.png" width="8" height="8"/>
+ </tile>
+ <tile id="50">
+  <image source="tile_0050.png" width="8" height="8"/>
+ </tile>
+ <tile id="51">
+  <image source="tile_0051.png" width="8" height="8"/>
+ </tile>
+ <tile id="52">
+  <image source="tile_0052.png" width="8" height="8"/>
+ </tile>
+ <tile id="53">
+  <image source="tile_0053.png" width="8" height="8"/>
+ </tile>
+ <tile id="54">
+  <image source="tile_0054.png" width="8" height="8"/>
+ </tile>
+ <tile id="55">
+  <image source="tile_0055.png" width="8" height="8"/>
+ </tile>
+ <tile id="56">
+  <image source="tile_0056.png" width="8" height="8"/>
+ </tile>
+ <tile id="57">
+  <image source="tile_0057.png" width="8" height="8"/>
+ </tile>
+ <tile id="58">
+  <image source="tile_0058.png" width="8" height="8"/>
+ </tile>
+ <tile id="59">
+  <image source="tile_0059.png" width="8" height="8"/>
+ </tile>
+ <tile id="60">
+  <image source="tile_0060.png" width="8" height="8"/>
+ </tile>
+ <tile id="61">
+  <image source="tile_0061.png" width="8" height="8"/>
+ </tile>
+ <tile id="62">
+  <image source="tile_0062.png" width="8" height="8"/>
+ </tile>
+ <tile id="63">
+  <image source="tile_0063.png" width="8" height="8"/>
+ </tile>
+ <tile id="64">
+  <image source="tile_0064.png" width="8" height="8"/>
+ </tile>
+ <tile id="65">
+  <image source="tile_0065.png" width="8" height="8"/>
+ </tile>
+ <tile id="66">
+  <image source="tile_0066.png" width="8" height="8"/>
+ </tile>
+ <tile id="67">
+  <image source="tile_0067.png" width="8" height="8"/>
+ </tile>
+ <tile id="68">
+  <image source="tile_0068.png" width="8" height="8"/>
+ </tile>
+ <tile id="69">
+  <image source="tile_0069.png" width="8" height="8"/>
+ </tile>
+ <tile id="70">
+  <image source="tile_0070.png" width="8" height="8"/>
+ </tile>
+ <tile id="71">
+  <image source="tile_0071.png" width="8" height="8"/>
+ </tile>
+ <tile id="72">
+  <image source="tile_0072.png" width="8" height="8"/>
+ </tile>
+ <tile id="73">
+  <image source="tile_0073.png" width="8" height="8"/>
+ </tile>
+ <tile id="74">
+  <image source="tile_0074.png" width="8" height="8"/>
+ </tile>
+ <tile id="75">
+  <image source="tile_0075.png" width="8" height="8"/>
+ </tile>
+ <tile id="76">
+  <image source="tile_0076.png" width="8" height="8"/>
+ </tile>
+ <tile id="77">
+  <image source="tile_0077.png" width="8" height="8"/>
+ </tile>
+ <tile id="78">
+  <image source="tile_0078.png" width="8" height="8"/>
+ </tile>
+ <tile id="79">
+  <image source="tile_0079.png" width="8" height="8"/>
+ </tile>
+ <tile id="80">
+  <image source="tile_0080.png" width="8" height="8"/>
+ </tile>
+ <tile id="81">
+  <image source="tile_0081.png" width="8" height="8"/>
+ </tile>
+ <tile id="82">
+  <image source="tile_0082.png" width="8" height="8"/>
+ </tile>
+ <tile id="83">
+  <image source="tile_0083.png" width="8" height="8"/>
+ </tile>
+ <tile id="84">
+  <image source="tile_0084.png" width="8" height="8"/>
+ </tile>
+ <tile id="85">
+  <image source="tile_0085.png" width="8" height="8"/>
+ </tile>
+ <tile id="86">
+  <image source="tile_0086.png" width="8" height="8"/>
+ </tile>
+ <tile id="87">
+  <image source="tile_0087.png" width="8" height="8"/>
+ </tile>
+ <tile id="88">
+  <image source="tile_0088.png" width="8" height="8"/>
+ </tile>
+ <tile id="89">
+  <image source="tile_0089.png" width="8" height="8"/>
+ </tile>
+ <tile id="90">
+  <image source="tile_0090.png" width="8" height="8"/>
+ </tile>
+ <tile id="91">
+  <image source="tile_0091.png" width="8" height="8"/>
+ </tile>
+ <tile id="92">
+  <image source="tile_0092.png" width="8" height="8"/>
+ </tile>
+ <tile id="93">
+  <image source="tile_0093.png" width="8" height="8"/>
+ </tile>
+ <tile id="94">
+  <image source="tile_0094.png" width="8" height="8"/>
+ </tile>
+ <tile id="95">
+  <image source="tile_0095.png" width="8" height="8"/>
+ </tile>
+ <tile id="96">
+  <image source="tile_0096.png" width="8" height="8"/>
+ </tile>
+ <tile id="97">
+  <image source="tile_0097.png" width="8" height="8"/>
+ </tile>
+ <tile id="98">
+  <image source="tile_0098.png" width="8" height="8"/>
+ </tile>
+ <tile id="99">
+  <image source="tile_0099.png" width="8" height="8"/>
+ </tile>
+ <tile id="100">
+  <image source="tile_0100.png" width="8" height="8"/>
+ </tile>
+ <tile id="101">
+  <image source="tile_0101.png" width="8" height="8"/>
+ </tile>
+ <tile id="102">
+  <image source="tile_0102.png" width="8" height="8"/>
+ </tile>
+ <tile id="103">
+  <image source="tile_0103.png" width="8" height="8"/>
+ </tile>
+ <tile id="104">
+  <image source="tile_0104.png" width="8" height="8"/>
+ </tile>
+ <tile id="105">
+  <image source="tile_0105.png" width="8" height="8"/>
+ </tile>
+ <tile id="106">
+  <image source="tile_0106.png" width="8" height="8"/>
+ </tile>
+ <tile id="107">
+  <image source="tile_0107.png" width="8" height="8"/>
+ </tile>
+ <tile id="108">
+  <image source="tile_0108.png" width="8" height="8"/>
+ </tile>
+ <tile id="109">
+  <image source="tile_0109.png" width="8" height="8"/>
+ </tile>
+ <wangsets>
+  <wangset name="big_tree_1" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="38" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="39" wangid="0,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
