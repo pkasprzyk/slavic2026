038f0053ba1f8239cc7a4eceeb76a305bc98056d
Grafiszti <grafiszti@gmail.com>
2026-08-08 11:18:23 +0200

    firefix

--- source/sprites.h
+++ source/sprites.h
@@ -41,7 +41,7 @@ void InitSprites(void);
 #define BUNNIES_MAX 10
 
 #define FIRE_OAM_BASE 11
-#define FIRE_CELLS_MAX 64
+#define FIRE_CELLS_MAX 40
 #define WATER_DROP_OAM_BASE (FIRE_OAM_BASE + FIRE_CELLS_MAX)
 #define WATER_DROP_MAX 5
 
